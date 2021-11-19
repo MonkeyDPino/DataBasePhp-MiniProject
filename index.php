eac68d675db01fb9de8d56623872318ed8c52a89
Juan Pino <juanrespolo@gmail.com>
2021-11-19 10:34:10 -0500

    final version

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/prueba.css">
-    <title>Prueba 1</title>
-</head>
-<body>
-    <a href="consultar_guardas.php">Consultar Guardas</a>
-    <a href="consultar_empresas.php">Consultar Empresas</a>
-    <a href="consultar_incidentes.php">Consultar Incidentes</a>
-    <a href="consultar_supervisores.php">Consultar Supervisores</a>
-    
-    <a href="./bootstrap_version/index.php">bootstrap</a>
+<?php include("./header.php")?>
 
-    
-</body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("./footer.php")?>
+
